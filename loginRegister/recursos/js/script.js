e2288bd280a5d52bf3daa78525c64488fd0ee25f
Paloma Pérez de Madrid <paloma.perezmadridlaguna@usp.ceu.es>
2023-08-03 11:21:41 +0200

    responsive design css

---
+++ loginRegister/recursos/js/script.js
@@ -0,0 +1,6 @@
+//Declarando variables
+var formulario_login = document.querySelector(".formulario__login");
+var formulario_register = document.querySelector(".formulario__register");
+var contenedor_login_register = document.querySelector(".contenedor__login-register");
+var caja_trasera_login = document.querySelector(".caja__trasera-login");
+var caja_trasera_register = document.querySelector(".caja__trasera-register");
